a09a074ff9d3070dd0684a673dc7c8a839ae1da9
Holger Cremer <HolgerCremer@gmail.com>
2017-03-08 12:27:54 +0100

    changes the config to 'live'

--- config.go
+++ config.go
@@ -2,10 +2,10 @@ package mfGalleryMetaCreatorGo
 
 const (
 	FILE_REGEXP          = `(?i)\.jpe?g$`
-	THUMB_DIR            = ".go_thumbs"
+	THUMB_DIR            = ".thumbs"
 	CONTENT_INI          = "content.ini"
-	META_NAME            = "go_meta.json"
-	META_NAME_CHROMECAST = "go_meta_cc.jsonp.js"
+	META_NAME            = "meta.json"
+	META_NAME_CHROMECAST = "meta_cc.jsonp.js"
 	CC_PREFIX            = "ifsImagesDataCallback("
 	CC_SUFFIX            = ");"
 )
